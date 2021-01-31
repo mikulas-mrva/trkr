d8cb1f31aac3130e28fd4fa5dcb6a8cab5eeec90
Mikulas Mrva <mikulas.mrva@gmail.com>
2021-01-31 02:18:06 +0100

    Fixing a mistake: created_at and modified_at were DateFields, but we need DateTimeFields

--- trkr/issues/models.py
+++ trkr/issues/models.py
@@ -67,8 +67,8 @@ class Issue(models.Model):
         default=timedelta(),
         null=False,
     )
-    created_at = models.DateField(auto_now_add=True, editable=False)
-    modified_at = models.DateField(auto_now=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
 
     def __str__(self):
         return f"#{self.pk} - {self.title} ({self.get_status_display()})"
